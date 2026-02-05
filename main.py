e624f579795b2778d011caf2c98d6401d11775b0
janusz-bit <janusz-bit@proton.me>
2026-02-05 07:55:09 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("No `api_key`")
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=args.user_prompt,
+    )
+    if (
+        response.usage_metadata.prompt_token_count == None
+        or response.usage_metadata.candidates_token_count == None
+    ):
+        raise RuntimeError("No metadata")
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
