016a53055e4fb5bdef72e112fb9343e83502b26c
janusz-bit <janusz-bit@proton.me>
2026-02-06 08:21:53 +0100

    Added content message part-feature

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
     )
     if (
         response.usage_metadata.prompt_token_count == None
